764684102044aec256871a865524d9c6d2e2ffa2
MelanieKastanie <74399952+MelanieKastanie@users.noreply.github.com>
2021-01-18 20:11:50 +0100

    Add files via upload
    Modul um die verbleibende Zeit als h:mm zu erhalten.
    
    Notwendige Package-Installation: sudo apt install ssocr
    Notwendige Anpassungen: Absolute Datenpfade im Modul müssen noch verändert werden.
    
    Mögliche Anpassung: print(data_neu) kann entfernt werden. War nur zur Überprüfung der Funktion.
    
    Verwendung: from GetTime import getTime

---
+++ GetTime.py
@@ -0,0 +1,21 @@
+"Modul zur Bestimmung der verbleibenden Restzeit der Waschmaschine"
+import os
+
+def getTime():
+	"Für /home/pi/share muss die entsprechende Adresse abgelegt werden, in der das Bild abgelegt wird und das output-file mit den gelesenen Daten"
+	os.system('ssocr -T -f white -b black -d -1 -C /home/pi/share/test_pi_3.jpeg > /home/pi/share/output')
+	f=open('/home/pi/share/output', 'r')
+	data = f.read()
+	"print(len(data))"
+	"print(data)"
+	"Ein ':' wird in den String eingefügt, damit an der Ausgabe nichts mehr verändert werden muss"
+	data_neu = data[0]+':'+data[1]+data[2]
+	print(data_neu)
+	return data_neu
+
+def main():
+	getTime()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
