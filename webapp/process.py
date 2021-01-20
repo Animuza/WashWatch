2c091368958fb51f46ddd3d2e794d8030297631f
Animuza <58681884+Animuza@users.noreply.github.com>
2021-01-20 23:17:30 +0100

    adding the basic push notification

--- webapp/process.py
+++ webapp/process.py
@@ -1,15 +1,39 @@
-# seed the pseudorandom number generator
+
 from random import seed
 from random import randint, random
 
+from flask import request, jsonify
+
+from app import PushSubscription
+from webpush_handler import trigger_push_notifications_for_subscriptions
+
+minutes = 0
+
+
+def trigger_push_notifications():
+    json_data = request.get_json()
+    subscriptions = PushSubscription.query.all()
+    results = trigger_push_notifications_for_subscriptions(
+        subscriptions,
+        'WashApp here!', 'The washing machine is ready!'
+    )
+    return jsonify({
+        "status": "success",
+        "result": results
+    })
+
 
 def example(test):
+    global minutes
+
     print(test)
     # seed random number generator
     seed(345)
     # generate some random numbers
     hours = randint(0, 4)
-    minutes = randint(0, 59)
+    minutes += 1
+    if minutes % 4 == 0:
+        trigger_push_notifications()
 
     print("Time left: " + str(hours) + ":" + str(minutes))
     return str(hours) + ":" + str(minutes)
